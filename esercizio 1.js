854737cafdcdbb4d5f7ae21564b783e270f67bdc
DragonautMk <michelesavarese1997@gmail.com>
2024-09-27 11:11:52 +0200

    inizializzato progetto, esercizi 1 e 2

---
+++ esercizio 1.js
@@ -0,0 +1,9 @@
+const pi =3.14;
+
+function cerchio(r) {
+    console.log(
+pi * r * r
+    );
+}
+
+cerchio(10);
---
+++ esercizio 2.js
@@ -0,0 +1,8 @@
+
+function ipotenusaconcateti(c1,c2) {
+    let i2 = (c1 * c1 + c2 * c2);
+    console.log(Math.sqrt(i2)
+    );
+}
+
+ipotenusaconcateti(3,4);
